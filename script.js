d7a469dbf6f0910e84e49ceb18c6051033cf3854
ParkourAG <guchhaitanubrata@gmail.com>
2025-05-29 23:21:45 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,101 @@
+async function getWeather(city) {
+    // function getWeather_emoji(val) {
+    //     if (val>=99) {
+    //         return "Thunderstorm with hail";
+    //     } else if(val>=90){
+    //         return "Thunderstorm: Light";
+    //     }
+    // }
+
+    //   0: "Clear sky",
+    //   1: "Mainly clear",
+    //   2: "Partly cloudy",
+    //   3: "Overcast",
+    //   45: "Fog",
+    //   48: "Fog (depositing)",
+    //   51: "Drizzle: Light",
+    //   61: "Rain: Light",
+    //   71: "Snow fall: Light",
+    //   80: "Rain showers: Light",
+    //   95: "Thunderstorm: Light",
+    //   99: "Thunderstorm with hail"
+    
+    try {
+        document.getElementById("heading-top").innerHTML="Weather of : "+ city_name.charAt(0).toUpperCase() + city_name.slice(1);
+
+        // Location of a city
+        const location_url = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(city)}`);
+        const location = await location_url.json();
+
+        if (!location.results || location.results.length === 0) {
+            console.log("❌ City not found.");
+            return;
+        }
+        const latitude= location.results[0].latitude;
+        const longitude= location.results[0].longitude;
+        const timezone= location.results[0].timezone;
+
+        // weather of city
+        // const weather_url= await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,weathercode,windspeed_10m_max&timezone=${timezone}`);
+        const weather_url= await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,weathercode,windspeed_10m_max,relative_humidity_2m_max,relative_humidity_2m_min&timezone=${timezone}`);
+        const weather= await weather_url.json();
+        // console.log(weather);
+        
+        //setting weathercode
+        document.querySelectorAll(".weathercode").forEach((div, index)=>{
+            div.innerHTML= weather.daily.weathercode[index +1];
+        })
+
+        //setting days
+        document.querySelectorAll(".day").forEach((div, index)=>{
+            div.innerHTML= weather.daily.time[index +1];
+        })
+
+        //setting max-min temperature
+        document.querySelectorAll(".max_temperature").forEach((div, index)=>{
+            div.innerHTML= weather.daily.temperature_2m_max[index +1] + weather.daily_units.temperature_2m_max;
+        })
+        document.querySelectorAll(".min_temperature").forEach((div, index)=>{
+            div.innerHTML= weather.daily.temperature_2m_min[index +1] + weather.daily_units.temperature_2m_min;
+        })
+
+        //setting precipitation_sum
+        document.querySelectorAll(".precipitation_sum").forEach((div, index)=>{
+            div.innerHTML= weather.daily.precipitation_sum[index +1] + weather.daily_units.precipitation_sum;
+        })
+
+        //setting windspeed_10m_max
+        document.querySelectorAll(".windspeed_10m_max").forEach((div, index)=>{
+            div.innerHTML= weather.daily.windspeed_10m_max[index +1] + weather.daily_units.windspeed_10m_max;
+        })
+
+        //setting relative_humidity_2m_max
+        document.querySelectorAll(".relative_humidity_2m_max").forEach((div, index)=>{
+            div.innerHTML= weather.daily.relative_humidity_2m_max[index +1] + weather.daily_units.relative_humidity_2m_max;
+        })
+
+
+    } catch (error) {
+        console.error("ERROR Getting Weather!!", error);
+    }
+}
+
+let city_name= "";
+
+// Accept value after pressing enter
+document.getElementById("city_name").addEventListener("keypress", function(div){
+    if(div.key=== "Enter"){
+        div.preventDefault();
+        city_name= div.target.value;
+        // console.log(`you entered: ${city_name}`);
+        getWeather(city_name);
+    }
+})
+
+// Accept value after pressing Search
+document.querySelector("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+  city_name = document.getElementById("city_name").value;
+//   console.log(`Submitted: ${city_name}`);
+  getWeather(city_name);
+});
---
+++ test.js
@@ -0,0 +1,64 @@
+{
+  latitude: 22.625,
+  longitude: 88.375,
+  generationtime_ms: 0.09226799011230469,
+  utc_offset_seconds: 19800,
+  timezone: 'Asia/Kolkata',
+  timezone_abbreviation: 'GMT+5:30',
+  elevation: 11,
+  daily_units: {
+    time: 'iso8601',
+    temperature_2m_max: '°C',
+    temperature_2m_min: '°C',
+    precipitation_sum: 'mm',
+    weathercode: 'wmo code',
+    windspeed_10m_max: 'km/h',
+    relative_humidity_2m_max: '%',
+    relative_humidity_2m_min: '%'
+  },
+  daily: {
+    time: [
+      '2025-05-29',
+      '2025-05-30',
+      '2025-05-31',
+      '2025-06-01',
+      '2025-06-02',
+      '2025-06-03',
+      '2025-06-04'
+    ],
+    temperature_2m_max: [
+      27.8, 30.9,
+      30.9, 29.4,
+      36.5, 36.2,
+      35.8
+    ],
+    temperature_2m_min: [
+      26.1, 25.9,
+      26.8, 26.7,
+      27.1, 27.5,
+      27.1
+    ],
+    precipitation_sum: [
+      57.2, 7.4, 23.9,
+        16, 0.3,  0.3,
+       4.8
+    ],
+    weathercode: [
+      95, 95, 96, 95,
+       3,  3, 96
+    ],
+    windspeed_10m_max: [
+       12, 12.2,  8.3,
+      7.2,    9, 10.8,
+        8
+    ],
+    relative_humidity_2m_max: [
+      97, 95, 96, 96,
+      96, 91, 97
+    ],
+    relative_humidity_2m_min: [
+      86, 75, 81, 82,
+      51, 51, 55
+    ]
+  }
+}
